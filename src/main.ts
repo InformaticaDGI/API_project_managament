b014df9d70488a2abc60e02e6ef60af6207ff126
Salvador Ron <salvadorronosio@gmail.com>
2025-09-21 17:42:47 -0400

    hotfix: remove prefix

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,6 @@ import { StateService } from './state/state.service';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.setGlobalPrefix('api');
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalPipes(new ValidationPipe({
     transform: true,
